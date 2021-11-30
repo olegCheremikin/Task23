aaf8cfe451b6607fbbf8a36516b9815f63055b47
O.Cheremikin <O.Cheremikin@CZC724BBKX>
2021-11-30 08:48:39 +0300

    Добавьте файлы проекта.

---
+++ Task23/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Numerics;
+
+namespace Task23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число, факториал которого нужно найти: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            BigInteger result = FactorialAsync(number).Result;
+            Console.WriteLine("Факториал числа = {0}", result);
+            Console.ReadKey();
+        }
+        static BigInteger Factorial(int number)
+        {
+            BigInteger factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= (BigInteger)i;
+                Thread.Sleep(500);
+            }
+            return (factorial);
+        }
+        static async Task<BigInteger> FactorialAsync(int number)
+        {
+            BigInteger result = await Task.Run(() => Factorial(number));
+            return result;
+        }
+    }
+}
